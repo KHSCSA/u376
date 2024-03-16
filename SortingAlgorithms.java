b0bfdbb12c185820fdb91471d9e982dd390908d2
heyscottn <heyscottn@gmail.com>
2024-03-16 08:43:08 -0500

    mm

---
+++ SortingAlgorithms.java
@@ -0,0 +1,48 @@
+import java.util.ArrayList;
+
+public class SortingAlgorithms{
+    /**
+     * Perform a selection sort an ArrayList of integers
+     * @param intList an ArrayList of integers
+     * @return intList sorted from smallest to largest
+     */
+    public static ArrayList<Integer> selectionSortInt(ArrayList<Integer> intList){
+        /* your code goes here */
+        
+        return intList;
+    }
+
+    /**
+     * Perform an insertion sort an ArrayList of integers
+     * @param intList an ArrayList of integers
+     * @return intList sorted from smallest to largest
+     */
+    public static ArrayList<Integer> insertionSortInt(ArrayList<Integer> intList){
+        /* your code goes here */
+
+        return intList;
+    }
+
+    /**
+     * Perform a selection sort an ArrayList of doubles
+     * @param doubleList an ArrayList of doubles
+     * @return doubleList sorted from smallest to largest
+     */ 
+    public static ArrayList<Double> selectionSortDouble(ArrayList<Double> doubleList){
+        /* your code goes here */
+        
+        return doubleList;
+    }
+
+    /**
+     * Perform an insertion sort an ArrayList of doubles
+     * @param doubleList an ArrayList of doubles
+     * @return doubleList sorted from smallest to largest
+     */
+    public static ArrayList<Double> insertionSortDouble(ArrayList<Double> doubleList){
+        /* your code goes here */
+        
+        return doubleList;
+    }
+
+}
